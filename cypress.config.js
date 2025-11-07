a3438733a99ac6ccb3dee103bd75df1d2e32a647
adelinop <adelinopessanha@hotmail.com>
2025-11-07 16:42:14 +0000

    enable cypress localhost

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,17 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Configurações de plugins aqui (se necessário)
     },
+    // Se estiver usando modo headless, passe o argumento pro navegador
+    browser: 'chrome',
   },
+  chromeWebSecurity: false,
+  video: false,
+  screenshotOnRunFailure: false,
+  viewportWidth: 1280,
+  viewportHeight: 720,
+  // Adiciona flags ao Chrome / Electron
+  userAgent: "CI",
+  env: {},
 });
